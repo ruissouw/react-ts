c8126dc8717a3cbd2b69bd8d4cb035c570511535
rohitaunn <rohit16@live.com>
2024-12-20 00:42:37 +0800

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './custom-components/Login/Login.tsx'
+import Navbar from './custom-components/Navbar/Navbar.tsx'
 
 const App: React.FC = () => {
 
   return (
     <>
+      <Navbar />
       <Login />
     </>
   )
---
+++ src/custom-components/Navbar/Navbar.tsx
@@ -0,0 +1,53 @@
+import { HStack } from "@chakra-ui/react"
+import { Button } from "@/components/ui/button"
+import { Flex } from "@chakra-ui/react"
+import './Navbar.css'
+
+const Username = () => {
+  return (
+    <HStack wrap="wrap" gap="6">
+      <Button variant="ghost" size="2xl">Username</Button>
+    </HStack>
+  )
+}
+
+const All_posts = () => {
+    return (
+      <HStack wrap="wrap" gap="6">
+        <Button variant="ghost" size="xl">All Posts</Button>
+      </HStack>
+    )
+  }
+
+const Following = () => {
+    return (
+      <HStack wrap="wrap" gap="6">
+        <Button variant="ghost" size="xl">Following</Button>
+      </HStack>
+    )
+}
+
+const Logout = () => {
+    return (
+      <HStack wrap="wrap" gap="6">
+        <Button variant="ghost" size="xl">Log out</Button>
+      </HStack>
+    )
+}
+
+const Navbar = () => {
+    return (
+      <div className="navbar-container">
+        <Flex gap="10">
+            <Username />
+            <All_posts />
+            <Following />
+            <Logout />
+        </Flex>
+      </div>
+    )
+}
+
+export default Navbar;
+
+
